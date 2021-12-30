977f9315b9555ae09b8eb5116630ce6f4124a7c8
Selvin Xue <xuexuedada@icloud.com>
2021-12-30 15:07:43 +0800

    refactor

--- src/Adapter/Sample1/PrintBanner.java
+++ src/Adapter/Sample1/PrintBanner.java
@@ -2,9 +2,13 @@ public class PrintBanner extends Banner implements Print {
     public PrintBanner(String string) {
         super(string);
     }
+
+    @Override
     public void printWeak() {
         showWithParen();
     }
+
+    @Override
     public void printStrong() {
         showWithAster();
     }
