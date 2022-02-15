9be4383d66ed162242ebe65967b09b9eefab91c6
Selvin Xue <xuexuedada@icloud.com>
2022-02-15 14:10:13 +0800

    refactor

--- src/Builder/Sample/Main.java
+++ src/Builder/Sample/Main.java
@@ -1,16 +1,20 @@
 public class Main {
+
+    public static final String PLAIN = "plain";
+    public static final String HTML = "html";
+
     public static void main(String[] args) {
         if (args.length != 1) {
             usage();
             System.exit(0);
         }
-        if (args[0].equals("plain")) {
+        if (args[0].equals(PLAIN)) {
             TextBuilder textbuilder = new TextBuilder();
             Director director = new Director(textbuilder);
             director.construct();
             String result = textbuilder.getResult();
             System.out.println(result);
-        } else if (args[0].equals("html")) {
+        } else if (args[0].equals(HTML)) {
             HTMLBuilder htmlbuilder = new HTMLBuilder();
             Director director = new Director(htmlbuilder);
             director.construct();
@@ -21,6 +25,7 @@ public class Main {
             System.exit(0);
         }
     }
+
     public static void usage() {
         System.out.println("Usage: java Main plain      编写纯文本文档");
         System.out.println("Usage: java Main html       编写HTML文档");
